45eea257e98c9fff52ff16919eb43230038bdc82
Abhiksha Goel <asnemaris@gmail.com>
2024-05-27 14:29:28 +0530

    ui changes for form

--- app/assetRequest/page.tsx
+++ app/assetRequest/page.tsx
@@ -169,7 +169,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Component() {
   return (
-    <div className="mx-auto max-w-md space-y-6 py-12">
+    <div className="mt-16 mx-auto max-w-md space-y-6">
       <div className="space-y-2 text-center">
         <h1 className="text-3xl font-bold">Request an Asset</h1>
         <p className="text-gray-500 dark:text-gray-400">
--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,8 @@ import Dashboard from "@/app/dashboard/page";
 
 export default function Home() {
   return (
-    <main>
-      <div className="mt-16 h-auto flex flex-col  md:ml-48">
-        <Dashboard />
-      </div>
+    <main className="mt-16 h-auto flex flex-col  md:ml-48">
+      <Dashboard />
     </main>
   );
 }
